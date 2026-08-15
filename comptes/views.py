78d1688910a4cbc94f83d176d9df91f96d0779fd
Methone <metheraste@gmail.com>
2026-08-15 07:25:53 +0100

    Refonte UI theme sombre + espace admin enrichi

--- comptes/views.py
+++ comptes/views.py
@@ -33,7 +33,32 @@ class DeconnexionView(LogoutView):
 
 @login_required
 def tableau_bord(request):
-    return render(request, 'comptes/tableau_bord.html')
+    from django.db.models import Sum
+    from tontines.models import Adhesion, Cotisation
+
+    adhesions = Adhesion.objects.filter(utilisateur=request.user, actif=True).select_related('tontine')
+
+    total_cotise = Cotisation.objects.filter(
+        adhesion__utilisateur=request.user,
+        statut=Cotisation.STATUT_PAYEE,
+    ).aggregate(total=Sum('montant'))['total'] or 0
+
+    en_attente = Cotisation.objects.filter(
+        adhesion__utilisateur=request.user,
+        statut__in=[Cotisation.STATUT_EN_ATTENTE, Cotisation.STATUT_EN_RETARD],
+    ).count()
+
+    journal = Cotisation.objects.filter(
+        adhesion__utilisateur=request.user,
+    ).select_related('cycle__tontine').order_by('-id')[:8]
+
+    return render(request, 'comptes/tableau_bord.html', {
+        'adhesions': adhesions,
+        'total_cotise': total_cotise,
+        'nombre_tontines': adhesions.count(),
+        'en_attente': en_attente,
+        'journal': journal,
+    })
 
 
 def est_admin(user):
@@ -42,6 +67,32 @@ def est_admin(user):
 
 @user_passes_test(est_admin, login_url='connexion')
 def espace_admin(request):
+    from django.db.models import Sum
     from .models import Utilisateur
-    membres = Utilisateur.objects.filter(role=Utilisateur.ROLE_MEMBRE).order_by('-date_creation')
-    return render(request, 'comptes/espace_admin.html', {'membres': membres})
+    from tontines.models import Tontine, Cotisation, Adhesion
+
+    membres = Utilisateur.objects.order_by('-date_creation')
+
+    tontines = Tontine.objects.all().order_by('-date_creation')
+    total_collecte = Cotisation.objects.filter(
+        statut=Cotisation.STATUT_PAYEE,
+    ).aggregate(total=Sum('montant'))['total'] or 0
+
+    cotisations_en_attente = Cotisation.objects.filter(
+        statut__in=[Cotisation.STATUT_EN_ATTENTE, Cotisation.STATUT_EN_RETARD],
+    ).count()
+
+    journal_global = Cotisation.objects.select_related(
+        'cycle__tontine', 'adhesion__utilisateur'
+    ).order_by('-id')[:15]
+
+    return render(request, 'comptes/espace_admin.html', {
+        'membres': membres,
+        'nombre_membres': membres.count(),
+        'tontines': tontines,
+        'nombre_tontines': tontines.count(),
+        'nombre_tontines_actives': tontines.filter(statut=Tontine.STATUT_ACTIVE).count(),
+        'total_collecte': total_collecte,
+        'cotisations_en_attente': cotisations_en_attente,
+        'journal_global': journal_global,
+    })
